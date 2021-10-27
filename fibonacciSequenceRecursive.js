b30371e220e47e47c303765d4b1f453dff2dafd0
smarteye <dotun.o@e360africa.com>
2021-10-27 06:16:50 +0100

    refactor fibonacci sequence

--- fibonacciSequenceRecursive.js
+++ fibonacciSequenceRecursive.js
@@ -4,6 +4,29 @@
  * @returns {Structure array} - Returns an array of fibonacci numbers up to specified depth
  */
 
-var makeFibonacciSequenceRecursive = function(depth) {
-    let f
-}
+
+let fibonacciArray=[];
+
+var makeFibonacciSequenceRecursive=function(depth) {
+
+    let currentArray = [0, 1]; //base array
+
+    if(depth<1) {
+
+        return [];
+
+    }
+
+    if(depth===1) {
+
+        return currentArray;
+
+    }
+
+    currentArray = makeFibonacciSequenceRecursive(depth-1);
+
+    return [...currentArray, currentArray[currentArray.length-1] + currentArray[currentArray.length-2] ]; // spread the current array, and add the last 2 elements together
+
+};
+
+console.log(makeFibonacciSequenceRecursive(25));
